e508faa3091d7ea9af48be0747b08532083aa4b8
Patrick de-SOUZA <patrick.de-souza1@unv-lorraine.fr>
2020-10-12 12:29:19 +0200

    Premier commit

---
+++ form1_show.php
@@ -0,0 +1,18 @@
+<?php
+    session_start();$_SESSION['form_id'] = uniqid();
+?>
+<html><head><meta charset="utf-8"><title>Form1</title></head><body>
+<?php
+    if( !empty($_SESSION['error_message'] )) {  
+        echo '<h2>' . implode('<br>',$_SESSION['error_message'] ) . '</h2>';
+        }
+?>
+<form  method="post" action="form2_submit.php">    
+    <p>      
+        <label for="nom">Nom : </label>      <input type="text" name="nom" id="nom">    </p>
+        <p>      <label for="prenom">Prénom : </label>      <input type="text" name="prenom" id="prenom">    </p>
+        
+        <p style="text-align: center">      <input type="submit" value="Valider">    </p>    
+        <input type="hidden" name="form_id"            value="<?php print $_SESSION['form_id'];?>">
+</form>
+</body></html>
